b1af186204eb572bd9b706a7e5015611ecc9b1af
Patrick Roelofs <contact@patrickroelofs.com>
2022-04-20 15:03:51 +0200

    attempt to add tailwind

---
+++ insideAirbnb/tailwind.config.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+    mode: 'jit',
+    purge: [
+        './wwwroot/index.html',
+        './**/*/.razor',
+    ],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
