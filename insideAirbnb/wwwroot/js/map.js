2b402cf22a8ba84a1d41a5a744329846a4a2f2ee
Patrick Roelofs <contact@patrickroelofs.com>
2022-04-24 23:58:32 +0200

    add mao

---
+++ insideAirbnb/wwwroot/js/map.js
@@ -0,0 +1,6 @@
+﻿mapboxgl.accessToken = 'pk.eyJ1IjoicGF0cmlja3JvZWxvZnMiLCJhIjoiY2wyN3k3MHR2MDQ1NjNrbDNxd3B4ZTliayJ9.pXSKWIwd8K__9fBqrz7BCw';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
