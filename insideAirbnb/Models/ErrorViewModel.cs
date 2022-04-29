a8c58ff8e129dfde191df65b271b92651c3f0979
Patrick Roelofs <contact@patrickroelofs.com>
2022-04-29 08:55:52 +0200

    remove backend

--- insideAirbnb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace InsideAirbnb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
