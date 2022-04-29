c94ff8d8772976ad8865e69cd86411651d541b7d
Patrick Roelofs <contact@patrickroelofs.com>
2022-04-29 15:42:05 +0200

    do i know what im doing? no, do i care? no

---
+++ insideAirbnb/Server/Controllers/neighbourhoodsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace insideAirbnb.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class neighbourhoodsController : ControllerBase
+    {
+        public static List<Neighbourhoods> neighbourhoods = new List<Neighbourhoods> { 
+            new Neighbourhoods { Neighbourhood = "Bijlmer-Centrum", NeighbourhoodGroup = null },
+            new Neighbourhoods { Neighbourhood = "Bijlmer-Oost", NeighbourhoodGroup = null },
+            new Neighbourhoods { Neighbourhood = "Bos en Lommer", NeighbourhoodGroup = null },
+            new Neighbourhoods { Neighbourhood = "Buitenveldert - Zuidas", NeighbourhoodGroup = null }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Neighbourhoods>>> GetNeighbourhoods()
+        {
+            return Ok(neighbourhoods);
+        }
+    }
+}
--- insideAirbnb/wwwroot/js/app.js
+++
@@ -1,8 +0,0 @@
-mapboxgl.accessToken =
-  "pk.eyJ1IjoicGF0cmlja3JvZWxvZnMiLCJhIjoiY2wyN3k3MHR2MDQ1NjNrbDNxd3B4ZTliayJ9.pXSKWIwd8K__9fBqrz7BCw";
-var map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
-  center: [4.902318081500628, 52.37851665631294],
-  zoom: 12,
-});
