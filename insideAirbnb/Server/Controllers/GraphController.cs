22a5d77622f78613560ae58a3b17888c075f709a
Patrick Roelofs <contact@patrickroelofs.com>
2022-05-17 17:01:47 +0200

    add graph stuff

---
+++ insideAirbnb/Server/Controllers/GraphController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace insideAirbnb.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GraphController : ControllerBase
+    {
+        private IGraphRepository _graphRepository { get;  } _listingRepository { get; }
+
+        public GraphController(IGraphRepository graphRepository)
+        {
+            _graphRepository = graphRepository;
+        }
+    }
+}
---
+++ insideAirbnb/Server/Repositories/GraphRepository.cs
@@ -0,0 +1,15 @@
+﻿using insideAirbnb.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace insideAirbnb.Server.Repositories
+{
+    public class GraphRepository : IGraphRepository
+    {
+        private readonly insideAirbnbContext _context;
+
+        public GraphRepository(insideAirbnbContext context)
+        {
+            _context = context;
+        }
+    }
+}
